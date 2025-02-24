e5db061226428e076b8993faec1071556d1044c6
pýrus <kayrus@users.noreply.github.com>
2025-02-24 11:35:08 +0100

    Introduce go-makefile-maker

---
+++ shell.nix
@@ -0,0 +1,22 @@
+# Copyright 2024 SAP SE
+# SPDX-License-Identifier: Apache-2.0
+
+{ pkgs ? import <nixpkgs> { } }:
+
+with pkgs;
+
+mkShell {
+  nativeBuildInputs = [
+    addlicense
+    ginkgo
+    go-licence-detector
+    go_1_24
+    golangci-lint
+    gotools # goimports
+    kubernetes-controller-tools # controller-gen
+    setup-envtest
+
+    # keep this line if you use bash
+    bashInteractive
+  ];
+}
